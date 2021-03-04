abdffb7fc90cd3f6e0d3613a662912959663bb20
jacquarg <guillaume.jacquart@hoodbrains.com>
2021-03-04 17:14:23 +0100

    Get informations about permissions stats.

--- privacymodulese/src/main/java/foundation/e/privacymodules/permissions/PermissionsPrivacyModule.kt
+++ privacymodulese/src/main/java/foundation/e/privacymodules/permissions/PermissionsPrivacyModule.kt
@@ -3,10 +3,13 @@ package foundation.e.privacymodules.permissions
 import android.app.AppOpsManager
 import android.app.AppOpsManager.*
 import android.content.Context
-import android.os.Build
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import foundation.e.privacymodules.commons.ManualAction
-import foundation.e.privacymodules.location.IFakeLocation
+import foundation.e.privacymodules.permissions.data.AppOpModes
+import foundation.e.privacymodules.permissions.data.ApplicationDescription
+import java.util.*
 
 /**
  * Implements [IPermissionsPrivacyModule] with all privileges of a system app.
@@ -17,49 +20,77 @@ class PermissionsPrivacyModule(context: Context): APermissionsPrivacyModule(cont
         = context.getSystemService(AppCompatActivity.APP_OPS_SERVICE) as AppOpsManager
 
     /**
-     * @see IPermissionsPrivacyModule.getPermissionStatus
-     * Return the status for the specified permission and app.
-     */
-    override fun getPermissionStatus(uid: Int, packageName: String, permissionName: String): PermissionStatus {
-        val opStr = AppOpsManager.permissionToOp(permissionName)
-        if (opStr != null) {
-            return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                val mode = appOpsManager.checkOpNoThrow(opStr, uid, packageName)
-                when (mode) {
-                    MODE_ALLOWED -> PermissionStatus.GRANTED
-                    else -> PermissionStatus.DENIED
-                }
-            } else {
-                val mode = appOpsManager.unsafeCheckOpRawNoThrow(opStr, uid, packageName)
-                when (mode) {
-                    MODE_ALLOWED -> PermissionStatus.GRANTED
-                    MODE_FOREGROUND -> PermissionStatus.FOREGROUND
-                    else -> PermissionStatus.DENIED
-                }
-            }
-        }
-        return PermissionStatus.DENIED
-    }
-
-    /**
-     * @see IPermissionsPrivacyModule.setRuntimePermission
+     * @see IPermissionsPrivacyModule.toggleDangerousPermission
      * Always return null, permission is set using privileged capacities.
      */
-    override fun setRuntimePermission(uid: Int,
-        packageName: String,
-        permissionName: String,
-        status: PermissionStatus
-    ): ManualAction? {
-        val op = permissionToOpCode(permissionName)
-        val toSet = when(status) {
-            PermissionStatus.GRANTED -> MODE_ALLOWED
-            PermissionStatus.FOREGROUND -> MODE_FOREGROUND
-            PermissionStatus.DENIED -> MODE_IGNORED
-        }
-        if (op != OP_NONE) {
-            appOpsManager.setMode(op, uid, packageName, toSet)
+    override fun toggleDangerousPermission(appDesc: ApplicationDescription,
+                                           permissionName: String,
+                                           grant: Boolean): ManualAction? {
+        try {
+            if (grant) {
+                context.packageManager.grantRuntimePermission(
+                    appDesc.packageName,
+                    permissionName,
+                    android.os.Process.myUserHandle()
+                )
+            } else {
+                context.packageManager.revokeRuntimePermission(
+                    appDesc.packageName,
+                    permissionName,
+                    android.os.Process.myUserHandle()
+                )
+            }
+        } catch (e: Exception) {
+            Toast.makeText(context, "Exception occured: ${e.message}", Toast.LENGTH_SHORT)
+            Log.e("Permissions-e", "Exception while setting permission", e)
         }
 
         return null
+    }
+
+    override fun setAppOpMode(
+        appDesc: ApplicationDescription,
+        permissionName: String,
+        mode: AppOpModes
+    ): ManualAction? {
+        val op = permissionToOpCode(permissionName)
+        if (op != OP_NONE) {
+            appOpsManager.setMode(op, appDesc.uid, appDesc.packageName, mode.modeValue)
+        }
+        return null
+    }
+
+    override fun isAccessibilityEnabled(): Boolean? = null
+
+    // WIP, get history of permissions use.
+    fun testPermissionHistory(appDesc: ApplicationDescription,
+                              permission: String) {
+        // when we change with setAppOpsPermissiosn ...
+//        appOpsManager.startWatchingMode(permissionToOp(permission), appDesc.packageName, OnOpChangedListener { op, packageName ->
+//            Log.d("WatchMode", "op: $op for! $packageName")
+//        })
+
+
+        val ops = appOpsManager.getOpsForPackage(appDesc.uid, appDesc.packageName,
+            OPSTR_CAMERA)
+        val tag = "TestGetOps"
+
+        Log.d(tag, "opsForPackage.size: ${ops.size}")
+
+        ops.forEach {
+            Log.d(tag, "pkg: ${it.packageName} - ops.size: ${it.ops.size}")
+            it.ops.forEach { opEntry ->
+                Log.d(tag, "time: ${Date(opEntry.time)} - duration: ${it.ops.firstOrNull()?.duration}ms, mode: ${opEntry.mode}, op: ${opEntry.opStr}")
+            }
+
+        }
+        // Not available in API 24.
+//        appOpsManager.startWatchingActive(
+//            arrayOf(permissionToOpCode(permission)).toIntArray(),
+//            //appDesc.packageName,
+//            OnOpActiveChangedListener { code, uid, packageName, active ->
+//                Log.d("WatchMode", "Op active: $active - op: ${opToName(code)} uid: $uid for $packageName")
+//            })
+
     }
 }
